728cfdf13d98dffe7a61ecc1f9e757f226060ebf
anands982 <anands9824@gmail.com>
2025-09-27 18:33:19 +0530

    added docker build and run in CI/CD pipeline

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ load_dotenv()
 app = Flask(__name__)
 
 def get_ssm_parameter(name):
-    ssm = boto3.client("ssm", region_name=os.getenv("AWS_DEFAULT_REGION", "ap-south-1"))
+    ssm = boto3.client("ssm", region_name=os.getenv("AWS_REGION", "ap-south-1"))
     response = ssm.get_parameter(Name=name, WithDecryption=True)
     return response["Parameter"]["Value"]
 
